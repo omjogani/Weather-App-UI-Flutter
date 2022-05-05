75f1414a6887f51bdac2b1bdbc0518f4a08fa7a6
Om Jogani <om080jogani@gmail.com>
2022-05-05 22:17:13 +0530

    initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_ui/screen/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather UI',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        fontFamily: "SF Pro Text",
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/screen/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: SvgPicture.asset(
+          "assets/images/Image.svg",
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
