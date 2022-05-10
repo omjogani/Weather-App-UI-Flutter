da55c6c16ffdc55de79582fadda21204570e1042
Om Jogani <om080jogani@gmail.com>
2022-05-10 22:10:00 +0530

    Changed Home Screen

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
@@ -98,65 +100,118 @@ class SlidingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SlidingUpPanel(
-      // backdropEnabled: ,
+      backdropEnabled: true,
+      backdropColor: Colors.black45,
+      backdropOpacity: 0.5,
+
       borderRadius: const BorderRadius.only(
         topLeft: Radius.circular(40.0),
         topRight: Radius.circular(40.0),
       ),
-      color: Colors.black87,
+      color: Colors.transparent,
+
       // boxShadow: <>[],
       minHeight: 285.0,
       maxHeight: MediaQuery.of(context).size.height * 0.75,
-      panel: Column(
+      panel: Stack(
         children: <Widget>[
-          const SizedBox(height: 10.0),
-          //indicator
-          Container(
-            width: 60.0,
-            height: 5.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(40.0),
+              topRight: Radius.circular(40.0),
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 6.0,
+                sigmaY: 6.0,
+              ),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.75,
+                decoration: const BoxDecoration(
+                  // border: Border(
+                  //   top: BorderSide(color: Colors.white, width: 2.0),
+                  // ),
+                  boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    offset: Offset(0, 1),
+                    color: Colors.white,
+                    spreadRadius: 1.0,
+                    blurRadius: 0.0,
+                  ),
+                ],
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                      Color(0xFF2E335A),
+                      Color(0xFF1C1B33),
+                    ],
+                  ),
+                ),
+                // color: Colors.black45,
+              ),
             ),
           ),
-          const SizedBox(height: 10.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const  <Widget>[
-              Text(
-                "Hourly Forecast",
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white54,
+          SlidingPanelBackground(),
+          Column(
+            children: <Widget>[
+              const SizedBox(height: 20.0),
+              //indicator
+              Container(
+                width: 60.0,
+                height: 5.0,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
-              Text(
-                "Weekly Forecast",
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white54,
+              const SizedBox(height: 10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const <Widget>[
+                  Text(
+                    "Hourly Forecast",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white54,
+                    ),
+                  ),
+                  Text(
+                    "Weekly Forecast",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white54,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              SizedBox(
+                height: 160.0,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: ListView(
+                    physics: const BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    children: const <Widget>[
+                      WeatherDetailCard(),
+                      WeatherDetailCard(),
+                      WeatherDetailCard(),
+                      WeatherDetailCard(),
+                      WeatherDetailCard(),
+                      WeatherDetailCard(),
+                      WeatherDetailCard(),
+                    ],
+                  ),
                 ),
               ),
+              const Spacer(),
+              // WeatherDetailCard(),
             ],
           ),
-          const SizedBox(height: 20.0),
-          SizedBox(
-            height: 160.0,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: const <Widget>[
-                WeatherDetailCard(),
-                WeatherDetailCard(),
-                WeatherDetailCard(),
-                WeatherDetailCard(),
-                WeatherDetailCard(),
-              ],
-            ),
-          ),
-          const Spacer(),
-          // WeatherDetailCard(),
         ],
       ),
     );
@@ -206,6 +261,73 @@ class WeatherDetailCard extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class SlidingPanelBackground extends StatelessWidget {
+  const SlidingPanelBackground({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          left: 50.0,
+          child: Container(
+            height: 150.0,
+            width: 150.0,
+            decoration: BoxDecoration(
+              gradient: RadialGradient(
+                colors: <Color>[
+                  Colors.yellow.withOpacity(0.3),
+                  Colors.yellow.withOpacity(0.2),
+                  Colors.yellow.withOpacity(0.1),
+                  Colors.yellow.withOpacity(0.01),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 100.0,
+          top: -100,
+          child: Container(
+            height: 250.0,
+            width: 250.0,
+            decoration: BoxDecoration(
+              gradient: RadialGradient(
+                colors: <Color>[
+                  Colors.pink.withOpacity(0.3),
+                  Colors.pink.withOpacity(0.2),
+                  Colors.pink.withOpacity(0.1),
+                  Colors.pink.withOpacity(0.01),
+                ],
+              ),
+            ),
+          ),
+        ),
+        // Positioned(
+        //   top: 0.0,
+        //   child: ClipRRect(
+        //     borderRadius: BorderRadius.circular(40.0),
+        //     child: Container(
+        //       height: 2.0,
+        //       width: MediaQuery.of(context).size.width,
+        //       decoration: BoxDecoration(
+        //         boxShadow: <BoxShadow>[
+        //           BoxShadow(
+        //             offset: Offset(0, 1),
+        //             color: Colors.white,
+        //             spreadRadius: 0.0,
+        //             blurRadius: 0.0,
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // ),
+      ],
     );
   }
 }
