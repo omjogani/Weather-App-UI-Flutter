b755b79ced95a38e03cf0c5e4809731e2dfe4ebf
Om Jogani <om080jogani@gmail.com>
2022-05-11 15:54:04 +0530

    Added Weather List Screen

---
+++ lib/screen/weather_list/weather_list_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WeatherListScreen extends StatelessWidget {
+  const WeatherListScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          // NavBar
+          // Main Weather List View
+        ],
+      ),
+    );
+  }
+}
+
